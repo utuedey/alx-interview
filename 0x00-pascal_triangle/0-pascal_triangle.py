6b7e4fa0d3e3a86ca7c16a0ed5c07c28ff53a2dc
utuedey <utuedeyejosph@gmail.com>
2023-07-02 21:43:37 +0000

    Add task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for creating a pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers
+       Representing the pascal's triangle of n.
+    """
+    triangle = []
+
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or  j == i:
+                line.append(1)
+            elif j > 0 or j > i:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
