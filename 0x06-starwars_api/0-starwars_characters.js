6c13519f11baf9c813ecf130dc13219c1876c275
utuedey <utuedeyejosph@gmail.com>
2023-08-10 22:22:24 -0700

    semi-standard validation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const baseUrl = "https://swapi-api.hbtn.io/api";
+const baseUrl = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2){
-  request(`${baseUrl}/films/${process.argv[2]}/`,(error, _, body) => {
-    if (error){
-	console.log(error);
+if (process.argv.length > 2) {
+  request(`${baseUrl}/films/${process.argv[2]}/`, (error, _, body) => {
+    if (error) {
+      console.log(error);
     }
     const charactersUrl = JSON.parse(body).characters;
     const charactersName = charactersUrl.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseError, _, RequestBody) => {
-	  if (promiseError){
-	    reject(promiseError);
-	  }
+        if (promiseError) {
+	  reject(promiseError);
+	}
 	  resolve(JSON.parse(RequestBody).name);
 	});
       }));
