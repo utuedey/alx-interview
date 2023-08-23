d89499e320181fdfd8d410856bc3ee9eeeac0697
utuedey <utuedeyejosph@gmail.com>
2023-08-23 05:58:13 +0000

    add #!/usr/bin/python3

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0-making_change module
+"""
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """Determines the fewest number of coins needed
+    to meet a given amount total.
+    Args:
+     - coins (List): list of the values of the coins in your possession
+     - total (int): total amount
+    Return:
+      - coins_count (int): fewest number of coins
+    """
+    if total <= 0:
+        return 0
+    remainder = total
+    coins_count = 0
+    coin_index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    num_of_coins = len(coins)
+
+    while remainder > 0:
+        if coin_index >= num_of_coins:
+            return -1
+        if remainder - sorted_coins[coin_index] >= 0:
+            remainder -= sorted_coins[coin_index]
+            coins_count += 1
+        else:
+            coin_index += 1
+    return coins_count
